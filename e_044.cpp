6e333a735adf2cd7df487dbc9041df760bdfc059
jtristao <joaovitortristao@gmail.com>
2019-04-17 07:12:38 -0300

    Add problem 44

---
+++ e_044.cpp
@@ -0,0 +1,39 @@
+/**************************************************************************
+	Author: João V. Tristão
+	Date:
+	Problem:
+	Approach:
+
+**************************************************************************/
+
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+#define N 10000
+
+void find_pentagonal_numbers(set<int>& p){
+	for(int i = 1; i < N; i++){
+		p.insert((i*(3*i-1))/2);
+	}
+}
+
+int main(int argc, char *argv[]){
+	set<int> pentagonal;
+	find_pentagonal_numbers(pentagonal);
+
+	int k;
+	int d;
+	for(set<int>::iterator i = pentagonal.begin(); i != pentagonal.end(); i++){
+		for(set<int>::iterator j = pentagonal.begin(); j != pentagonal.end(); j++){
+			k = *i + *j;
+			d = abs(*j - *i);
+			if(pentagonal.count(k) == 1 && pentagonal.count(d) == 1){
+				printf("%d + %d = %d\n", *i, *j, k);
+				printf("%d - %d = %d\n", *i, *j, d);
+			}
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
